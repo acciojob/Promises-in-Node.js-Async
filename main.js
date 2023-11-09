8db5194505ea7831fa88a3a0c1114ed244b9fd95
thelastenvoy <satvikbatra@gmail.com>
2023-11-09 13:59:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,16 +2,29 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(`Error reading file ${filePath}: ${err}`);
-    return;
-  }
+function readFileAsync(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(`Error reading file ${filePath}: ${err}`);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
 
-  try {
-    const jsonData = JSON.parse(data);
-    // TODO: Perform error handling for invalid file format and missing data
-  } catch (err) {
-    console.error('Invalid JSON file format. Please provide a valid JSON file.');
-  }
-});
+function modifyText(text) {
+  return new Promise((resolve, reject) => {
+    // TODO: Convert text to uppercase and reverse it
+  });
+}
+
+readFileAsync(filePath)
+  .then((data) => modifyText(data))
+  .then((modifiedText) => {
+    console.log(modifiedText);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
